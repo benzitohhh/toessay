d0a5609c7445ff9bbe126c73771a0e1ae186b6e6
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2012-11-04 17:55:11 -0300

    removed some junk from sidebar

--- wordpress/wp-content/themes/sight/sidebar.php
+++ wordpress/wp-content/themes/sight/sidebar.php
@@ -7,18 +7,19 @@
         );
     ?>
 
-    <?php the_widget( 'GetConnected', 'title=Get Connected', $widget_args); ?>
+    <?php //the_widget( 'GetConnected', 'title=Get Connected', $widget_args); ?>
 
     <?php the_widget( 'Recentposts_thumbnail', 'title=Recent posts', $widget_args); ?>
 
-    <div class="widget sponsors">
-        <h3>Our sponsors</h3>
-        <div class="widget-body">
-            <a href="#"><img src="<?php bloginfo('template_url'); ?>/example/sponsor01.jpg" width="200" height="125" alt=""/></a>
-            <a href="#"><img src="<?php bloginfo('template_url'); ?>/example/sponsor02.jpg" width="200" height="125" alt=""/></a>
-            <a href="#"><img src="<?php bloginfo('template_url'); ?>/example/sponsor03.jpg" width="200" height="125" alt=""/></a>
-        </div>
-    </div>
+
+    <!-- <div class="widget sponsors"> -->
+    <!--     <h3>Our sponsors</h3> -->
+    <!--     <div class="widget-body"> -->
+    <!--         <a href="#"><img src="<?php bloginfo('template_url'); ?>/example/sponsor01.jpg" width="200" height="125" alt=""/></a> -->
+    <!--         <a href="#"><img src="<?php bloginfo('template_url'); ?>/example/sponsor02.jpg" width="200" height="125" alt=""/></a> -->
+    <!--         <a href="#"><img src="<?php bloginfo('template_url'); ?>/example/sponsor03.jpg" width="200" height="125" alt=""/></a> -->
+    <!--     </div> -->
+    <!-- </div> -->
             
     <?php endif; ?>
 </div>
