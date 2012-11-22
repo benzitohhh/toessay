facce3a95bcf56cad5e8f3418976daa5663f499e
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2012-11-21 22:40:26 -0300

    new local db dump

--- wordpress/wp-content/themes/toessay-theme/category.php
+++ wordpress/wp-content/themes/toessay-theme/category.php
@@ -1,15 +1,107 @@
+
+
+
 <?php get_header(); ?>
 
-<div class="content-title">
 
+<?php
+global $query_string, $wpdb, $post;
+
+// get ALL posts
+$posts = query_posts($query_string.'&showposts=-1');
+
+// get ranked posts
+$cat_id = get_queried_object()->term_id;
+$querystr = "
+    SELECT * FROM $wpdb->posts
+    LEFT JOIN $wpdb->postmeta ON($wpdb->posts.ID = $wpdb->postmeta.post_id)
+    LEFT JOIN $wpdb->term_relationships ON($wpdb->posts.ID = $wpdb->term_relationships.object_id)
+    LEFT JOIN $wpdb->term_taxonomy ON($wpdb->term_relationships.term_taxonomy_id = $wpdb->term_taxonomy.term_taxonomy_id)
+    LEFT JOIN $wpdb->terms ON($wpdb->term_taxonomy.term_id = $wpdb->terms.term_id)
+    WHERE $wpdb->terms.term_id = $cat_id
+    AND $wpdb->term_taxonomy.taxonomy = 'category'
+    AND $wpdb->posts.post_status = 'publish'
+    AND $wpdb->posts.post_type = 'post'
+    AND $wpdb->postmeta.meta_key = 'rank'
+    ORDER BY $wpdb->postmeta.meta_value+0 ASC
+";
+$ranked_posts = $wpdb->get_results($querystr, OBJECT);
+
+// push unranked posts into ranked
+foreach ($ranked_posts as $post) {
+    $ids[] = $post->ID;
+}
+foreach ($posts as $post) {
+    if (!in_array($post->ID, $ids)) {
+        $ranked_posts[] = $post;
+    }
+}
+$N = count($ranked_posts);
+
+?>
+
+<div class="content-title">
     <?php $post = $posts[0]; // Hack. Set $post so that the_date() works.
           printf(__('%s'), single_cat_title('', false)); ?>
-
     <a href="javascript: void(0);" id="mode"<?php if ($_COOKIE['mode'] == 'grid') echo ' class="flip"'; ?>></a>
 </div>
 
-<?php get_template_part('loop'); ?>
+<?php if ($ranked_posts): ?>
 
-<?php get_template_part('pagination'); ?>
+    <div id="loop" class="<?php if ($_COOKIE['mode'] == 'grid') echo 'grid'; else echo 'list'; ?> clear">
+    
+        <div class="hero">
+            <?php $post = $ranked_posts[0]; ?>
+            <?php setup_postdata($post); ?>
+            <div <?php post_class('post clear'); ?> id="post_<?php the_ID(); ?>">
+                <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+                <div class="post-meta">
+                    <span class="post-author"><a
+                        href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" title="Posts by <?php the_author(); ?>"><?php the_author(); ?></a></span>
+                </div>
+                <div class="post-content"><?php if (function_exists('smart_excerpt')) smart_excerpt(get_the_excerpt(), 55); ?> <a href="<?php the_permalink() ?>">More</a></div>
+            </div>
+        </div>
+
+        <?php if ($ranked_posts[1]): ?>
+            <div class="subhero">
+                <?php for ($i = 1; $i < 3 && $i < $N; $i++): ?>
+                    <?php $post = $ranked_posts[$i]; ?>
+                    <?php setup_postdata($post); ?>
+                    <div <?php post_class('post clear'); ?> id="post_<?php the_ID(); ?>">
+                        <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+                        <div class="post-meta">
+                            <span class="post-author"><a
+                                href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" title="Posts by <?php the_author(); ?>"><?php the_author(); ?></a></span>
+                        </div>
+                        <div class="post-content"><?php if (function_exists('smart_excerpt')) smart_excerpt(get_the_excerpt(), 55); ?> <a href="<?php the_permalink() ?>">More</a></div>
+                    </div>
+                <?php endfor;?>
+            </div>
+        <?php endif;?>
+
+        <?php if ($ranked_posts[3]): ?>
+            <div class="morelinks">
+                <div class="col1">
+                    <?php for ($i = 3; $i < 7 && $i < $N; $i++): ?>
+                        <?php $post = $ranked_posts[$i]; ?>
+                        <?php setup_postdata($post); ?>
+                        <div <?php post_class('post clear'); ?> id="post_<?php the_ID(); ?>">
+                                <span class="post-author"><a
+                                    href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" title="Posts by <?php the_author(); ?>"><?php the_author(); ?></a></span>
+                                <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+                                <a href="<?php the_permalink() ?>"> More</a>
+                        </div>
+                    <?php endfor;?>
+                </div>
+                <div class="col2">
+                    
+                </div>
+            </div>
+        <?php endif;?>
+        
+    </div>
+
+<?php endif; ?>
 
 <?php get_footer(); ?>
--- wordpress/wp-content/themes/toessay-theme/single.php
+++ wordpress/wp-content/themes/toessay-theme/single.php
@@ -4,20 +4,20 @@
         <?php while ( have_posts() ) : the_post(); ?>
 
         <div class="content-title">
-            <?php the_category(' <span>/</span> '); ?>
-            <a href="http://facebook.com/share.php?u=<?php the_permalink() ?>&amp;t=<?php echo urlencode(the_title('','', false)) ?>" target="_blank" class="f" title="Share on Facebook"></a>
-            <a href="http://twitter.com/home?status=<?php the_title(); ?> <?php echo getTinyUrl(get_permalink($post->ID)); ?>" target="_blank" class="t" title="Spread the word on Twitter"></a>
-            <a href="http://digg.com/submit?phase=2&amp;url=<?php the_permalink() ?>&amp;title=<?php the_title(); ?>" target="_blank" class="di" title="Bookmark on Del.icio.us"></a>
-            <a href="http://stumbleupon.com/submit?url=<?php the_permalink() ?>&amp;title=<?php echo urlencode(the_title('','', false)) ?>" target="_blank" class="su" title="Share on StumbleUpon"></a>
+            <!-- <?php the_category(' <span>/</span> '); ?> -->
+            <!-- <a href="http://facebook.com/share.php?u=<?php the_permalink() ?>&amp;t=<?php echo urlencode(the_title('','', false)) ?>" target="_blank" class="f" title="Share on Facebook"></a> -->
+            <!-- <a href="http://twitter.com/home?status=<?php the_title(); ?> <?php echo getTinyUrl(get_permalink($post->ID)); ?>" target="_blank" class="t" title="Spread the word on Twitter"></a> -->
+            <!-- <a href="http://digg.com/submit?phase=2&amp;url=<?php the_permalink() ?>&amp;title=<?php the_title(); ?>" target="_blank" class="di" title="Bookmark on Del.icio.us"></a> -->
+            <!-- <a href="http://stumbleupon.com/submit?url=<?php the_permalink() ?>&amp;title=<?php echo urlencode(the_title('','', false)) ?>" target="_blank" class="su" title="Share on StumbleUpon"></a> -->
         </div>
 
         <div class="entry">
             <div <?php post_class('single clear'); ?> id="post_<?php the_ID(); ?>">
                 <div class="post-meta">
                     <h1><?php the_title(); ?></h1>
-                    by <span class="post-author"><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" title="Posts by <?php the_author(); ?>"><?php the_author(); ?></a></span> on <span
-                        class="post-date"><?php the_time(__('M j, Y')) ?></span> &bull; <span><?php the_time() ?></span> <?php edit_post_link( __( 'Edit entry'), '&bull; '); ?><a
-                        href="#comments" class="post-comms"><?php comments_number(__('No Comments'), __('1 Comment'), __('% Comments'), '', __('Comments Closed') ); ?></a></div>
+                    <span class="post-author"><a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>" title="Posts by <?php the_author(); ?>"><?php the_author(); ?></a></span>
+                    <?php edit_post_link( __( 'Edit entry'), '&bull; '); ?>
+                </div>
                 <div class="post-content"><?php the_content(); ?></div>
                 <div class="post-footer"><?php the_tags(__('<strong>Tags: </strong>'), ', '); ?></div>
             </div>
