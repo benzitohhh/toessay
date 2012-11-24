d4c732ade4fbe963ffe4ed25765d647b2471f5d9
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2012-11-24 18:13:05 -0300

    set width back to 960px, 5 cols

--- wordpress/wp-content/themes/toessay-theme/issuebar.php
+++ wordpress/wp-content/themes/toessay-theme/issuebar.php
@@ -1,7 +1,7 @@
                 <!-- issuebar -->
                 <div class="issuebar">
                     <div class="holder">
-                        <img src="http://placehold.it/132x132">
+                        <img src="http://placehold.it/154x154">
                         <p><?php echo $cat_date; ?>, <?php echo $cat_name; ?></p>
                     </div>
                 </div>
