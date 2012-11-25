21bfef8e4630bbabf2f7480c663dda37b26382e8
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2012-11-25 18:53:46 -0300

    404 message

--- wordpress/wp-content/themes/toessay-theme/404.php
+++ wordpress/wp-content/themes/toessay-theme/404.php
@@ -1,18 +1,9 @@
 <?php get_header(); ?>
 
-
-<div class="content-title">
-    404! We couldn't find the page!
-</div>
-
-<div class="entry">
-    <div <?php post_class('single clear'); ?> id="post_<?php the_ID(); ?>">
-        <div class="post-content">
-            <p>The page you've requested can not be displayed. It appears you've missed your intended destination, either through a bad or outdated link, or a typo in the page you were hoping to reach.</p>
-        </div>
+<div class="404-page">
+    <div class="content-title">
+        Sorry we couldn't find the page you requested.
     </div>
 </div>
-
-
 
 <?php get_footer(); ?>
