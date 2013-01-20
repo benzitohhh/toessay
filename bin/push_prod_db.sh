35fedcbe9e178faf5ec10b1b456a8e7e32ecb756
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2013-01-20 16:25:33 -0300

    push_prod script working

--- bin/push_prod_db.sh
+++ bin/push_prod_db.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
 #dump
-#cd ~/Desktop/toessay/db;
-#mysqldump wp-toessay-local -uroot -proot > dump-local.sql;
+cd ~/Desktop/toessay/db;
+mysqldump wp-toessay-local -uroot -proot > dump-local.sql;
 
 #scp
-#scp dump-local.sql toessayc@toessay.co.uk:~/toessay/db/dump-local.sql;
-#rm -rf dump-local.sql;
+scp dump-local.sql toessayc@toessay.co.uk:~/toessay/db/dump-local.sql;
+rm -rf dump-local.sql;
+
+DAY=$(( `date +%Y-%m-%d-%H%M-%s` ));
+BACKUPFILE=backups/wp-toessay-prod-$DAY.sql;
 
 ssh toessayc@toessay.co.uk \
    "\
     cd ~/toessay/db; \
-    #backup \
-    mkdir -p backups; \
-    #day=$(( `date +%s` )); \
-    #mysqldump -utoessayc -pNelsonmandela123! toessayc_wp-toessay-prod > backups/wp-toessay-prod-backup-$day.sql; \
+    mysqldump -utoessayc -pNelsonmandela123! toessayc_wp-toessay-prod > $BACKUPFILE; \
     \
-    #convert and apply \
-    #sed 's/http:\/\/toessay.co.uk.ben/http:\/\/www.toessay.co.uk/g' dump-local.sql > dump-local-prod.sql; \
-    #mysql -utoessayc -pNelsonmandela123! toessayc_wp-toessay-prod < dump-local-prod.sql; \
+    sed 's/http:\/\/toessay.co.uk.ben/http:\/\/www.toessay.co.uk/g' dump-local.sql > dump-local-prod.sql; \
+    mysql -utoessayc -pNelsonmandela123! toessayc_wp-toessay-prod < dump-local-prod.sql; \
     \
-    #cleanup \
-    #rm -rf dump*; \
+    rm -rf dump*; \
    ";
 
